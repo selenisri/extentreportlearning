70d02753ffdd7a7763bfbeb3aaa35f81e907bff2
BerlinZaigo <berlin@zaigoinfotech.com>
2025-01-06 21:57:17 +0530

    first commit

--- src/main/java/PageObjects/AddUserPage.java
+++ src/main/java/PageObjects/AddUserPage.java
@@ -8,39 +8,31 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddUserPage {
-	
-	static WebDriver  driver;
-	
+
+	static WebDriver driver;
 
 	By np = By.id("password");
 
-	    public AddUserPage(WebDriver driver) {
-	        this.driver = driver;
-	    }
-	    
-	    public static void Sendkeys(By Element , String Text) {
-	    	
-	    	WebDriverWait wait = new WebDriverWait(driver, 20);
-	 	    WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(Element));
-	 	    emailField.sendKeys(Text);
-	    }
-	    
-	    public static void Enter()
-	    {
-	    	driver.findElement(By.id("exampleInputEmail1")).sendKeys("Hello");
-	    }
-	    
-	    
-	    
-	    
-	    public void EnterText(String value)
-	    {
-	    	if(value.equals("NewPassword")) {
-	    		this.Sendkeys(np, "Hello@123");
-	    	}
-	    	
-	    }
-	    
-	    
+	public AddUserPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public static void Sendkeys(By Element, String Text) {
+
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(Element));
+		emailField.sendKeys(Text);
+	}
+
+	public static void Enter() {
+		driver.findElement(By.id("exampleInputEmail1")).sendKeys("Hello");
+	}
+
+	public void EnterText(String value) {
+		if (value.equals("NewPassword")) {
+			this.Sendkeys(np, "Hello@123");
+		}
+
+	}
 
 }
